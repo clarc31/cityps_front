7f2f1628a884ab11e9763a16d7eb50dee754b8d6
sendamdb <sendameddeb@outlook.fr>
2022-12-15 15:53:24 +0100

    Frontend_senda_V4_Home_screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useState, useEffect } from 'react';
 import MapView from 'react-native-maps'; // initialRegion permet de gérer le positionnement par défaut de la carte.
-import { Marker } from 'react-native-maps'; // a utiliser plus tard pour les markers
-import * as Location from 'expo-location'; // GÉOLOCALISATION
+import { Marker } from 'react-native-maps'; // pour les markers
+import * as Location from 'expo-location'; // pour la GÉOLOCALISATION
+
 
 import {
   StyleSheet,
@@ -11,72 +12,142 @@ import {
   View,
   TouchableOpacity,
   Image,
-  Dimensions,
+  TextInput,
+  ScrollView,
 } from 'react-native';
-
-import { SearchBar } from 'react-native-elements';
-
 
 
 export default function HomeScreen({ navigation }) {
 
-  // search bar:
-  const [search, setSearch] = useState("");
-  const updateSearch = (search) => {
-  setSearch(search);
+// géolocalisation (carte d'accueil du user):
+
+    const [location, setLocation] = useState({});
+  
+    useEffect(() => {
+      (async () => {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+  
+        if (status === 'granted') {
+          Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High},
+            (location) => {
+              setLocation(location.coords.accuracy);
+              
+            });
+        }
+      })();
+    }, []);
+
+
+// SEARCH BAR :
+
+const searchBar = () => {
+
+  // 1) create a variable using the useState() hook to take the input:
+  const [searchCity, setSearchCity] = useState("");
+
+ /* 2) Create a handler function that will read changes in the search bar.:
+
+const handleChange = (e) => {
+  e.preventDefault();
+  setSearchCity(e.target.value);
+};
+*/
+
+/* 3) Check if city written in the searchbar exist in API :
+
+data.features.length = {
+name: city.properties.city,
+latitude: city.geometry.coordinates[1],
+longitude: city.geometry.coordinates[0],
 };
 
 
-  /*
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.value);
-
-  const [city, setCity] = useState('');
-
-  // Modifiez le fonctionnement de PlacesScreen afin que lorsqu’une nouvelle ville est ajoutée (depuis l’input reliée 
-  // à l’API Adresses), la route POST /places soit appelée afin de l’enregistrer en base de données.
-
-  const handleSubmit = () => {
-    if (city.length === 0) {
-      return;
-    }
-
-// 1st request: get geographic data from API
+1st request: get geographic data from API
 fetch(`https://api-adresse.data.gouv.fr/search/?q=${city}`)
 .then((response) => response.json())
 .then((data) => {
-  // Nothing is done if no city is found by API
+
+  // Create an if statement that will return the city that match what has been entered into the search bar:
+
+  // a) Nothing is done if no city is found by API
   if (data.features.length === 0) {
     return;
   }
-
-const fistCity = data.features[0];
-const newPlace = {
-name: fistCity.properties.city,
-latitude: fistCity.geometry.coordinates[1],
-longitude: fistCity.geometry.coordinates[0],
-};
-
+  
+// b) A city is found by API:
+  if (data.features.length > 0) {
+    city.filter((city) => {
+    return city.name.match(searchCity);
+    setSearchCity(location.coords.accuracy);
+});
 */
 
-  // géolocalisation dans un composant
-  const [location, setLocation] = useState({});
+};
+
+// List of selected tips by catégories for this City for this user:
+
+const selectedTipsForThisUser = () => {
+/*
+=> Créer une route GET existingTipsbyCity (qui affiche tous les tips dispo sur l'appli toutes categories confondues)
+
+useEffect(() => {
+    fetch(`http://localhost:3000/tips/:cityname`)
+      .then(response => response.json())
+      .then((data) => {
+        data.result && dispatch(addTiptoSelection(data.tips));
+      });
+   }, []);
+
+addTiptoSelection est un reducer (un tableau) 
+
+=> Créer une route GET selectedCategorieByUSer (qui affiche les categories selectionnées par le user)
+useEffect(() => {
+    fetch(`http://localhost:3000/user/:categories`)
+      .then(response => response.json())
+      .then((data) => {
+        data.result && dispatch(addCategoriestoSelection(data.user);
+      });
+   }, []);
+
+addCategoriestoSelection est un reducer (un tableau)
+
+const existingTipsForThisUser = existingTipsbySelectedCategories.match(searchCity);
+*/
+
+return (
+    <View style={styles.tipsList}>
+        <Image source={require('../assets/avatar.png')}  style={styles.avatar} /> // {user.avatar}
+        <Text style={styles.title}> {tips.title} </Text>
+        <Text style={styles.place}> {tips.city} </Text>
+        <Text style={styles.content}> {tips.description} </Text>     
+    </View>
+    );
+};
+
+
+// markers:
+
+/*  
+  const otherTipsforThisCity = [existingTipsbyCity-selectedTipsForThisUser].match(searchCity);
   
+  const orangeMarkers = existingTipsForThisUser.map((data, i) => {
+    return <><Marker key={i} coordinate={{ latitude: data.latitude, longitude: data.longitude }} /><Icon name='location-pin' color='#F77B55' /> </> 
+  });
+
+  const blackMarkers = otherTipsforThisCity.map((data, i) => {
+    return <><Marker key={i} coordinate={{ latitude: data.latitude, longitude: data.longitude }} /><Icon name='location-pin' color='#475059' /> </> 
+  });
+
   useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
+    fetch(`http://localhost:3000/tips/:cityname`)
+      .then(response => response.json())
+      .then((data) => {
+        data.result && dispatch(addMarkers({orangeMarkers + blackMarkers});
+      });
+   }, []);
 
-      if (status === 'granted') {
-        Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High},
-          (location) => {
-            setLocation(location.coords.accuracy);
-            
-          });
-      }
-    })();
-  }, []);
-
-
+   AddMarkers est un reducer (un tableau)
+*/
 
   return (
   <View style={styles.mainContainer}>
@@ -85,16 +156,26 @@ longitude: fistCity.geometry.coordinates[0],
 
   <View style={styles.rowContainer}>
 
-<SearchBar 
-lightTheme
-searchIcon={{ size: 24 }}
-showCancel
-containerStyle={{backgroundColor: 'white', width: 300, heigth: 10, justifyContent: 'center', borderWidth: 0, borderRadius: 10}}
-placeholder="Indiquez votre ville..."
-onChangeText={updateSearch}
-value={search}
->
-</SearchBar>
+  <View style={styles.searchBar}>
+    <TextInput
+    //value={searchInput}
+    //onChange={handleChange}
+  
+      style={{
+        borderRadius: 10,
+        margin: 10,
+        color: '#000',
+        borderColor: '#666',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        height: 45,
+        paddingHorizontal: 10,
+        fontSize: 18,
+      }}
+      placeholder={'Indiquez votre ville...'}
+      placeholderTextColor={'#666'}
+    />
+ </View>
 
 <TouchableOpacity >
 <Icon name='bars' size={40} color='#475059' style={styles.menuIcon}/>
@@ -107,25 +188,18 @@ value={search}
       showsUserLocation={true}
       followsUserLocation={true}
       >
-        
-
         <Marker coordinate={location} title="My position" pinColor="#fecb2d" />
      
     </MapView>
     
   </View>   
+  
 
-    <View style={styles.bottomContainer}>
-    
-        <View style={styles.tipsList}>
-        <Image source={require('../assets/avatar.png')}  style={styles.avatar} /> 
-        <Text style={styles.title}> Titre </Text>
-        <Text style={styles.place}> Lieu </Text>
-        <Text style={styles.content}> Premières lignes du descriptyps</Text>
-        
-    </View>
+  <ScrollView contentContainerStyle={styles.bottomContainer}>
+        {selectedTipsForThisUser}
+  </ScrollView>
 
-  </View>
+ 
 
   </View>
 
@@ -143,19 +217,26 @@ const styles = StyleSheet.create({
    padding: 0,
   },
 
-  
+  /*
   
   topContainer: {
     //justifyContent: 'space-between',
     flex: 1.2,
-    marginTop: 0,
+    top: 0,
     width: '100%',
-  },
-
+    backgroundColor: 'red',
+   
+  },*/
+  
   rowContainer: {
   flexDirection :'row',
   justifyContent: 'space-around',
-  marginTop: '12%',
+  //position: 'relative',
+  top: 50, 
+  },
+
+  searchBar: {
+  width: '80%',
   },
   
   menuIcon: {
@@ -166,6 +247,7 @@ const styles = StyleSheet.create({
   map: {
       flex: 1.2,
       marginTop: 0,
+      //position: 'relative',
     },
    
   bottomContainer: {
