6234e215297b261a54fa776e0034029ac4bd5d99
sendamdb <sendameddeb@outlook.fr>
2022-12-19 12:24:48 +0100

    front_homescreen_mise en page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,6 @@ import MapView from 'react-native-maps'; // initialRegion permet de gérer le po
 import { Marker } from 'react-native-maps'; // pour les markers
 import * as Location from 'expo-location'; // pour la GÉOLOCALISATION
 
-
 import {
   StyleSheet,
   Text,
@@ -16,27 +15,28 @@ import {
   ScrollView,
 } from 'react-native';
 
-
 export default function HomeScreen({ navigation }) {
 
 // géolocalisation (carte d'accueil du user):
 
     const [location, setLocation] = useState({});
+    console.log(location)
   
     useEffect(() => {
       (async () => {
         const { status } = await Location.requestForegroundPermissionsAsync();
   
         if (status === 'granted') {
-          Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High},
+          // Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High},
+          Location.watchPositionAsync({ distanceInterval: 10 },
             (location) => {
-              setLocation(location.coords.accuracy);
+              console.log("location", location)
+              setLocation({latitude : location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.08,longitudeDelta:0.08});
               
             });
         }
       })();
     }, []);
-
 
 // SEARCH BAR :
 
@@ -45,7 +45,7 @@ const searchBar = () => {
   // 1) create a variable using the useState() hook to take the input:
   const [searchCity, setSearchCity] = useState("");
 
- /* 2) Create a handler function that will read changes in the search bar.:
+ /* 2) Create a handler function that will read changes in the searchbar:
 
 const handleChange = (e) => {
   e.preventDefault();
@@ -116,7 +116,7 @@ const existingTipsForThisUser = existingTipsbySelectedCategories.match(searchCit
 
 return (
     <View style={styles.tipsList}>
-        <Image source={require('../assets/avatar.png')}  style={styles.avatar} /> // {user.avatar}
+        <Image source={require('../assets/avatar.png')}  style={styles.avatar} />  {/* {user.avatar} */}
         <Text style={styles.title}> {tips.title} </Text>
         <Text style={styles.place}> {tips.city} </Text>
         <Text style={styles.content}> {tips.description} </Text>     
@@ -149,136 +149,110 @@ return (
    AddMarkers est un reducer (un tableau)
 */
 
+   const scrollTest = []
+   for(let i = 0; i < 10; i++) {
+    scrollTest.push(     <View style={styles.tipsList} key={i}>
+      <Image source={require('../assets/avatar.png')}  style={styles.avatar} />
+      <View style={styles.insideCardContainer}>
+        <View style={styles.topCarte}>
+          <Text style={styles.title}> Titre </Text>
+          <Text style={styles.place}> Lieu </Text>
+        </View>
+        <Text style={styles.content}> Premières lignes du descriptyps</Text>
+
+      </View>
+      
+      </View>)
+   }
+
   return (
-    <View style={styles.mainContainer}>
-   {/* <View style={styles.topContainer}>*/}
-      <View style={styles.rowContainer}>
-
-        {/* <SearchBar 
-
-        {/*<SearchBar 
->>>>>>> front16
-          lightTheme
-          searchIcon={{ size: 24 }}
-          showCancel
-          containerStyle={{backgroundColor: 'white', width: 300, heigth: 10, justifyContent: 'center', borderWidth: 0, borderRadius: 10}}
-          placeholder="Indiquez votre ville..."
-          onChangeText={updateSearch}
-          value={search}
-          >
-
-        </SearchBar> */}
-
-
-
-        <TouchableOpacity >
-          <Icon name='bars' size={40} color='#475059' style={styles.menuIcon}/>
-        </TouchableOpacity>
-      {/*  </View>*/}
-    <MapView 
-      Type="hybrid" style={styles.map} title="My location" pinColor='#FED579'
-      showsUserLocation={true}
-      followsUserLocation={true}
-      >
-      {/* <Marker coordinate={location} title="My position" pinColor="#fecb2d" />*/}     
-    </MapView>   
-  </View>   
- {/* <View style={styles.bottomContainer}> 
-    <View style={styles.tipsList}>
-      <Image source={require('../assets/avatar.png')}  style={styles.avatar} /> 
-      <Text style={styles.title}> Titre </Text>
-      <Text style={styles.place}> Lieu </Text>
-      <Text style={styles.content}> Premières lignes du descriptyps</Text>  
-</View>
-  </View>*/}
-</View>
-
-)};
-
-/* Init Senda
- return (
   <View style={styles.mainContainer}>
 
-  <View style={styles.topContainer}>
+    <View style={styles.topContainer}>
 
-  <View style={styles.rowContainer}>
+      <View style={styles.rowContainer}>
 
-  <View style={styles.searchBar}>
-    <TextInput
-    //value={searchInput}
-    //onChange={handleChange}
-  
-      style={{
-        borderRadius: 10,
-        margin: 10,
-        color: '#000',
-        borderColor: '#666',
-        backgroundColor: '#FFF',
-        borderWidth: 1,
-        height: 45,
-        paddingHorizontal: 10,
-        fontSize: 18,
-      }}
-      placeholder={'Indiquez votre ville...'}
-      placeholderTextColor={'#666'}
-    />
- </View>
+        <View style={styles.searchBar}>
+          <TextInput
+          //value={searchInput}
+          //onChange={handleChange}
+            style={{
+              borderRadius: 10,
+              margin: 10,
+              color: '#000',
+              borderColor: '#666',
+              backgroundColor: '#FFF',
+              borderWidth: 1,
+              height: 45,
+              paddingHorizontal: 10,
+              fontSize: 18,
+            }}
+            placeholder={'Indiquez votre ville...'}
+            placeholderTextColor={'#666'}
+            searchIcon={{ size: 24 }}
+          />
+        </View>
 
-<TouchableOpacity >
-<Icon name='bars' size={40} color='#475059' style={styles.menuIcon}/>
-</TouchableOpacity>
+        <TouchableOpacity >
+            <Icon name='bars' size={40} color='#475059' style={styles.menuIcon}/>
+        </TouchableOpacity>
 
-</View>
+      </View>
 
-    <MapView 
-      Type="hybrid" style={styles.map} title="My location" pinColor='#FED579'
-      showsUserLocation={true}
-      followsUserLocation={true}
-      >
+      <MapView 
+       style={styles.map}
+        initialRegion={{
+          latitude: 47.88,
+          longitude: 2.51,
+          latitudeDelta: 8,
+          longitudeDelta: 18,
+        }}
+        region={location}
+        >
         <Marker coordinate={location} title="My position" pinColor="#fecb2d" />
-     
-    </MapView>
+      </MapView>
+    </View>  
+
+     <ScrollView contentContainerStyle={styles.scrollContainer} className={styles.bottomContainer}>
+     {/* {selectedTipsForThisUser} */}
     
-  </View>   
-  
+            {scrollTest}
+     </ScrollView>
 
-  <ScrollView contentContainerStyle={styles.bottomContainer}>
-        {selectedTipsForThisUser}
-  </ScrollView>
+  </View> 
 
- 
+  )};
 
-  </View>
 
-)};
-*/
 
 const styles = StyleSheet.create({
 
   mainContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center', 
-    backgroundColor:"red", 
-   margin: 0,
-   padding: 0,
+  flex: 1,
+  // alignItems: 'center',
+  // justifyContent: 'center',  
+  //  margin: 0,
+  //  padding: 0,
+   backgroundColor: '#77D0DE',
   },
 
-  /*
-  
   topContainer: {
-    //justifyContent: 'space-between',
-    flex: 1.2,
-    top: 0,
-    width: '100%',
+    // justifyContent: 'space-between',
+    // flex: 1.2,
+    // top: 0,
+    // width: '100%',
     backgroundColor: 'red',
-   
-  },*/
+    height: '45%',
+    position: 'relative',
+  },
   
   rowContainer: {
-    height:"100%",
   flexDirection :'row',
   justifyContent: 'space-around',
+  alignItems: 'center',
+  position: 'absolute',
+  top: 40, 
+  zIndex: 1,
   },
 
   searchBar: {
@@ -290,23 +264,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  map: {
-      flex: 1,
-      //position: 'relative',
+ map: {
+    flex: 1,
     },
    
   bottomContainer: {
-    flex: 0.8,
-    backgroundColor: '#77D0DE',
-    padding: 0,
-    width: '100%',
+    // height: '100%',
+    // padding: 0,
+    // width: '100%',
+    // backgroundColor: 'blue',
   },
-
-
+  scrollContainer: {
+    // height: '100%',
+    backgroundColor: '#77D0DE',
+  },
   tipsList:  {
-    alignItems: 'flex-start',
-    flexDirection: "column",
-    flexWrap: "wrap",
+    // alignItems: 'flex-start',
+    // flexDirection: "column",
+    // flexWrap: "wrap",
+    flexDirection: "row",
+    borderRadius: 10,
+    borderColor: "#F77B55",
+    borderWidth: 2,
+    margin: 4,
+    backgroundColor: "#D6F5FA",
+    alignItems: "center",
+    height: 80
+  },
+  topCarte: {
+    // height: 60,
+    flexDirection: 'row',
+    // justifyContent: 'flex-between',
+
+  },
+  insideCardContainer: {
+    flexDirection: 'column',
   },
 
   avatar: {
