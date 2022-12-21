0c136162cb815160487e10b3a40e393bf564ea6a
sendamdb <sendameddeb@outlook.fr>
2022-12-21 11:07:12 +0100

    front_homeScreen_debug

--- App.js
+++ App.js
@@ -3,21 +3,47 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import HomeScreen from './screens/HomeScreen';
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import DesCriptypScreen from './screens/DesCriptypScreen'; // Ajout import Screen
 import DisplayUserAlbumScreen from './screens/DisplayUserAlbumScreen';
 import NewPostScreen from './screens/NewPostScreen';
+
+/* REDUX PERSIST POUR REACT NATIVE */
+
+//  1) imports nécessaires pour utiliser redux-persist:
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import user from './reducers/user'
-// import user from './reducers/user';
-const reducers = ({user});
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { persistStore, persistReducer  } from 'redux-persist'; 
+import { PersistGate } from 'redux-persist/integration/react'; 
+
+// 2) Importez également la fonction combineReducers :
+import { configureStore, combineReducers} from '@reduxjs/toolkit'; 
+
+// 3) Importez le reducer:
+import user from './reducers/user'; 
+
+// 3) Définissez le ou les reducer(s) de votre application avec la fonction combineReducers :
+const reducers = combineReducers({ user });
+
+// 4) Utilisez une clé de stockage pour définir un nom à votre store à l’intérieur du local storage :
+const persistConfig = {
+  key: 'cityps',
+  storage: AsyncStorage,
+}
+
+// 5) Mettez ensuite à jour le contenu de votre store avec la fonction configureStore :
 const store = configureStore({
-  reducer: { },
-});
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+ });
+ 
+// 6) Transformez votre store pour qu’il soit persistant :
+const persistor = persistStore(store)
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -59,6 +85,7 @@ export default function App() {
   
   return (
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <NavigationContainer >
         <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
           <Stack.Screen name="TabNavigator" component={TabNavigator} />
@@ -70,6 +97,7 @@ export default function App() {
           <Stack.Screen name="SignIn"component={SignInScreen} />
         </Stack.Navigator>
       </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
